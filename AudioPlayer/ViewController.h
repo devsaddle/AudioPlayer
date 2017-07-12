543d0454979d3db0d4c45c2be57acf3dda4af60d
马远 <coder_ma@163.com>
2017-07-12 15:06:15 +0800

    Initial Commit

---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by 马远 on 2017/7/12.
+//  Copyright © 2017年 Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AudioPlayer
+//
+//  Created by 马远 on 2017/7/12.
+//  Copyright © 2017年 Yuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
